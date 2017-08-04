38d4b71bfd58fa3f0165e5d622e6e155da5d679e
hongda <hongda.li@hp.com>
2017-08-04 10:34:29 +0800

    first commit

---
+++ src/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Posts extends Component {
+  render() {
+    const id = this.props.match.params.id
+    return (
+    <div>
+        <h2>This is the post section: {id}</h2>
+    </div>
+    );
+  }
+}
+
+
+export default Posts;
