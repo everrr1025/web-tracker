a310853b01e1f9bb201f17d719b50c4535e1f483
hongda <hongda.li@hp.com>
2017-08-08 15:32:47 +0800

    integrate redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import starterApp from "./reducers";
+import App from "./components/App";
 import { BrowserRouter as Router } from "react-router-dom";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./utils/registerServiceWorker";
+//import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
+const store = createStore(starterApp);
+
+//onst history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
+
 registerServiceWorker();
